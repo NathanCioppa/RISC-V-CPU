c1d202c42fa397249a7d4fae10df893b7805cce7
Nathan Cioppa <nathan.cioppa@gmail.com>
2026-07-26 14:44:58 -0400

    finished alu and jumper

--- alu.v
+++ alu.v
@@ -1,32 +1,53 @@
 
 `timescale 1ns/1ps
+
 `include "alu_codes.vh"
+`include "mem_codes.vh"
+`include "size_codes.vh"
 
 module alu #(
 	parameter XLEN = 32;
+	parameter XREG_COUNT = 32;
 )(
 	input clk,
-	input [XLEN-1:0] alu_lhs, alu_rhs,
-	input [$clog(ALU_CODES_COUNT)-1:0] alu_op,
-	output reg [XLEN-1:0] result
+	
+	input [$clog(ALU_CODES_COUNT)-1:0] alu_code,
+	input [XLEN-1:0] op_lhs, op_rhs,
+	input [$clog(XREG_COUNT)-1:0] rd,
+	input [$clog(MEM_CODES_COUNT)-1:0] mem_code,
+	input [$clog(SIZE_CODES_COUNT)-1:0] size_code, 
+
+	output reg [XLEN-1:0] out_result,
+	output reg [$clog(XREG_COUNT)-1:0] out_rd,
+	output reg out_do_wb,
+	output reg [$clog(SIZE_CODES_COUNT)-1:0] out_size_code, 
+	output reg [$clog(MEM_CODES_COUNT)-1:0] out_mem_code
 ); 
 
-always @(posedge clk) begin
-	case (alu_op)
-		ALU_ADD: result <= alu_lhs + alu_rhs;
-		ALU_SUB: result <= alu_lhs - alu_rhs;
-		ALU_AND: result <= alu_lhs & alu_rhs; 
-		ALU_OR: result <= alu_lhs | alu_rhs;
-		ALU_XOR: result <= alu_lhs ^ alu_rhs;
-		ALU_SLL: result <= alu_lhs << alu_rhs;
-		ALU_SRL: result <= alu_lhs >> alu_rsh;
-		ALU_SRA: result <= $signed(alu_lhs) >>> alu_rhs;
-		ALU_SLT: result <= $signed(alu_lhs) < $signed(alu_rhs)? 1:0;
-		ALU_SLTU: result <= alu_lhs > alu_rhs ? 1:0;
-		ALU_AUI: result <= alu_lhs + alu_rhs << 12;
-	endcase
-end
+wire do_wb;
 
+assign do_wb = (alu_code != ALU_INVALID) && (mem_code == MEM_INVALID);
+
+always @(posedge clk) begin
+	case (alu_code)
+		ALU_ADD: result <= op_lhs + op_rhs;
+		ALU_SUB: result <= op_lhs - op_rhs;
+		ALU_AND: result <= op_lhs & op_rhs; 
+		ALU_OR: result <= op_lhs | op_rhs;
+		ALU_XOR: result <= op_lhs ^ op_rhs;
+		ALU_SLL: result <= op_lhs << op_rhs;
+		ALU_SRL: result <= op_lhs >> alu_rsh;
+		ALU_SRA: result <= $signed(op_lhs) >>> op_rhs;
+		ALU_SLT: result <= $signed(op_lhs) < $signed(op_rhs)? 1:0;
+		ALU_SLTU: result <= op_lhs > op_rhs ? 1:0;
+		ALU_AUI: result <= op_lhs + (op_rhs << 12);
+	endcase
+
+	out_rd <= rd;
+	out_do_wb <= do_wb;
+	out_size_code <= size_code;
+	out_mem_code <= mem_code;
+end
 
 endmodule
 
--- jumper.v
+++ jumper.v
@@ -1,15 +1,18 @@
 
 `timescale 1ns/1ps;
+
 `include "jumper_codes.vh"
 
 module jumper #(
-parameter XLEN = 32
-) (
-input clk,
-input [XLEN-1:0] add_lhs, add_rhs,
-input [XLEN-1:0] cmp_lhs, cmp_rhs,
-input [$clog(JUMPER_CODES_COUNT)-1:0] cond
-output reg [XLEN-1:0] pc;
+	parameter XLEN = 32
+)(
+	input clk,
+	input [XLEN-1:0] add_lhs, add_rhs,
+	input [XLEN-1:0] cmp_lhs, cmp_rhs,
+	input [$clog(JUMPER_CODES_COUNT)-1:0] jumper_code,
+
+	output reg [XLEN-1:0] out_pc,
+	output reg result_valid
 );
 
 wire [XLEN-1:0] sum;
@@ -17,17 +20,21 @@ wire do_assignment;
 
 assign sum = add_lhs + add_rhs;
 
-assign do_assignment = (cond == JUMP_UNCOND) 
-	|| ( (cond == JUMP_EQ) && (cmp_lhs == cmp_rhs) )
-	|| ( (cond == JUMP_NEQ) && (cmp_lhs != cmp_rhs) )
-	|| ( (cond == JUMP_LT) && ( $signed(cmp_lhs) < $signed(cmp_rhs) ) )
-	|| ( (cond == JUMP_GTE) && ( $signed(cmp_lhs) >= $signed(cmp_rhs) ) )
-	|| ( (cond == JUMP_U_LT) && (cmp_lhs < cmp_rhs) )
-	|| ( (cond == JUMP_U_GTE) && (cmp_lhs >= cmp_rhs) );
+assign do_assignment = (jumper_code == JUMP_UNCOND) 
+	|| ( (jumper_code == JUMP_EQ) && (cmp_lhs == cmp_rhs) )
+	|| ( (jumper_code == JUMP_NEQ) && (cmp_lhs != cmp_rhs) )
+	|| ( (jumper_code == JUMP_LT) && ( $signed(cmp_lhs) < $signed(cmp_rhs) ) )
+	|| ( (jumper_code == JUMP_GTE) && ( $signed(cmp_lhs) >= $signed(cmp_rhs) ) )
+	|| ( (jumper_code == JUMP_U_LT) && (cmp_lhs < cmp_rhs) )
+	|| ( (jumper_code == JUMP_U_GTE) && (cmp_lhs >= cmp_rhs) );
 
 always @(posedge clk) begin
-	if(do_assignment)
-		pc <= sum;
+	if(do_assignment) begin
+		out_pc <= sum;
+		result_valid <= 1;
+	end
+	else
+		result_valid <= 0;
 end
 
 
